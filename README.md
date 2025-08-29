4056f7e9c04e5ed1bb4de699f5c3c6807248f45f
Your Name <vemasani.pavani@gmail.com>
2025-08-29 23:53:22 +0530

    rds

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ docdb for mongodb
 elasticache for redis
 rabbitmq ---> normal ec2
 
+
+
+
+
+docdb:
+------
+Download the Amazon DocumentDB Certificate Authority (CA) certificate required to authenticate to your cluster
+==============================================================================================================
+wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+
+Connect to this cluster with the mongo shell
+==============================================
+mongosh docdb-cluster-dev.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile global-bundle.pem --retryWrites=false --username docdb --password <insertYourPassword>
+
+Connect to this cluster with an application
+============================================
+mongodb://docdb:<insertYourPassword>@docdb-cluster-dev.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false
--- main.tf
+++ main.tf
@@ -5,41 +5,41 @@
 #   env              =   var.env
 #   zone_id          =   var.zone_id
 # }
-module "docdb" {
-  for_each            =    var.docdb
-  source             =    "./module/docdb"
-  cluster_identifier =    each.value["cluster_identifier"]
-  engine             =    each.value["engine"]
-  engine_version     =    each.value["engine_version"]
-  family             =    each.value["family"]
-  instance_class      =   each.value["instance_class"]
-  subnet_id          =    module.VPC.backend
-  server_app_ports   =    var.dbServers
-  env                =    each.value["env"]
-  docdb_username     =    "docdb"
-  docdb_password     =    "roboshop123"
-  kms_key_id         =    each.value["kms_key_id"]
-  vpc_id             =    module.VPC.vpc_id
-}
-# module "rds" {
-#   source                = "./module/rds"
-#   for_each              = var.rds
-#   allocated_storage     = each.value["allocated_storage"]
-#   component             = each.value["db_name"]
-#   engine                = each.value["engine"]
-#   engine_version        = each.value["engine_version"]
-#   env                   = var.env
-#   family                = each.value["family"]
-#   instance_class        = each.value["instance_class"]
-#   kms_key_id            = var.kms_key_id
-#   server_app_ports      = var.dbServers
-#   subnet_id             = module.VPC.backend
-#   vpc_id                = module.VPC.vpc_id
-#   multi_az              = false
-#   publicly_accessible   = false
-#   skip_final_snapshot   = true
-#   storage_type          = true
+# module "docdb" {
+#   for_each            =    var.docdb
+#   source             =    "./module/docdb"
+#   cluster_identifier =    each.value["cluster_identifier"]
+#   engine             =    each.value["engine"]
+#   engine_version     =    each.value["engine_version"]
+#   family             =    each.value["family"]
+#   instance_class      =   each.value["instance_class"]
+#   subnet_id          =    module.VPC.backend
+#   server_app_ports   =    var.dbServers
+#   env                =    each.value["env"]
+#   docdb_username     =    "docdb"
+#   docdb_password     =    "roboshop123"
+#   kms_key_id         =    each.value["kms_key_id"]
+#   vpc_id             =    module.VPC.vpc_id
 # }
+module "rds" {
+  source                = "./module/rds"
+  for_each              = var.rds
+  allocated_storage     = each.value["allocated_storage"]
+  component             = each.value["db_name"]
+  engine                = each.value["engine"]
+  engine_version        = each.value["engine_version"]
+  env                   = var.env
+  family                = each.value["family"]
+  instance_class        = each.value["instance_class"]
+  kms_key_id            = var.kms_key_id
+  server_app_ports      = var.dbServers
+  subnet_id             = module.VPC.backend
+  vpc_id                = module.VPC.vpc_id
+  multi_az              = false
+  publicly_accessible   = false
+  skip_final_snapshot   = true
+  storage_type          = true
+}
 # module "redis"{
 #   source           = "./module/redis"
 #   for_each         = var.redis
