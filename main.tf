a1756411796dba4de35147202ba585061b58461c
Your Name <vemasani.pavani@gmail.com>
2025-08-29 23:40:53 +0530

    rds,redis,elasticache,rabbitmq,docdb

--- main.tf
+++ main.tf
@@ -21,52 +21,52 @@ module "docdb" {
   kms_key_id         =    each.value["kms_key_id"]
   vpc_id             =    module.VPC.vpc_id
 }
-module "rds" {
-  source                = "./module/rds"
-  for_each              = var.rds
-  allocated_storage     = each.value["allocated_storage"]
-  component             = each.value["db_name"]
-  engine                = each.value["engine"]
-  engine_version        = each.value["engine_version"]
-  env                   = var.env
-  family                = each.value["family"]
-  instance_class        = each.value["instance_class"]
-  kms_key_id            = var.kms_key_id
-  server_app_ports      = var.dbServers
-  subnet_id             = module.VPC.backend
-  vpc_id                = module.VPC.vpc_id
-  multi_az              = false
-  publicly_accessible   = false
-  skip_final_snapshot   = true
-  storage_type          = true
-}
-module "redis"{
-  source           = "./module/redis"
-  for_each         = var.redis
-  cluster_id       = each.value["cluster_id"]
-  engine           = each.value["engine"]
-  engine_version   = each.value["engine_version"]
-  server_app_ports  = var.dbServers
-  env              = var.env
-  node_type        = each.value["node_type"]
-  num_cache_nodes  = each.value["num_cache_nodes"]
-  family           = each.value["family"]
-  subnet_id        = module.VPC.backend
-  vpc_id           = module.VPC.vpc_id
-}
-module "rabbitmq"{
-  source           = "./module/rabbitmq"
-  for_each         = var.rabbitmq
-  component        = each.value["component"]
-  env              = var.env
-  instance_type    = each.value["instance_type"]
-  zone_id          = var.zone_id
-  bastion_node    = var.bastion_node
-  vpc_id           = module.VPC.vpc_id
-  subnet_id        = var.dbServers
-  kms_key_id       = var.kms_key_id
-  volume_type      = var.volume_type
-}
+# module "rds" {
+#   source                = "./module/rds"
+#   for_each              = var.rds
+#   allocated_storage     = each.value["allocated_storage"]
+#   component             = each.value["db_name"]
+#   engine                = each.value["engine"]
+#   engine_version        = each.value["engine_version"]
+#   env                   = var.env
+#   family                = each.value["family"]
+#   instance_class        = each.value["instance_class"]
+#   kms_key_id            = var.kms_key_id
+#   server_app_ports      = var.dbServers
+#   subnet_id             = module.VPC.backend
+#   vpc_id                = module.VPC.vpc_id
+#   multi_az              = false
+#   publicly_accessible   = false
+#   skip_final_snapshot   = true
+#   storage_type          = true
+# }
+# module "redis"{
+#   source           = "./module/redis"
+#   for_each         = var.redis
+#   cluster_id       = each.value["cluster_id"]
+#   engine           = each.value["engine"]
+#   engine_version   = each.value["engine_version"]
+#   server_app_ports  = var.dbServers
+#   env              = var.env
+#   node_type        = each.value["node_type"]
+#   num_cache_nodes  = each.value["num_cache_nodes"]
+#   family           = each.value["family"]
+#   subnet_id        = module.VPC.backend
+#   vpc_id           = module.VPC.vpc_id
+# }
+# module "rabbitmq"{
+#   source           = "./module/rabbitmq"
+#   for_each         = var.rabbitmq
+#   component        = each.value["component"]
+#   env              = var.env
+#   instance_type    = each.value["instance_type"]
+#   zone_id          = var.zone_id
+#   bastion_node    = var.bastion_node
+#   vpc_id           = module.VPC.vpc_id
+#   subnet_id        = var.dbServers
+#   kms_key_id       = var.kms_key_id
+#   volume_type      = var.volume_type
+# }
 module "VPC"{
   source                       = "./module/VPC"
   env                          = var.env
