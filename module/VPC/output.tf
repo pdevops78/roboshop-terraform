6efddc00f07f88d43115f6127c26dc13060b4edd
Your Name <vemasani.pavani@gmail.com>
2025-08-30 19:31:06 +0530

    vpc

--- module/VPC/output.tf
+++ module/VPC/output.tf
@@ -7,9 +7,9 @@ value = aws_subnet.public_subnets.*.id
 output "frontend"{
 value = aws_subnet.frontend_subnets.*.id
 }
-output "db"{
-value = aws_subnet.db_subnets.*.id
-}
-output "backend"{
-value = aws_subnet.backend_subnets.*.id
-}
+# output "db"{
+# value = aws_subnet.db_subnets.*.id
+# }
+# output "backend"{
+# value = aws_subnet.backend_subnets.*.id
+# }
